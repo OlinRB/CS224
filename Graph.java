ecd46fe208f2cc7a8261fd04dc672fe238f790e4
OlinRB <olinrb@live.com>
2022-02-19 20:58:38 -0500

    cleaned up

--- Graph.java
+++ Graph.java
@@ -2,14 +2,17 @@ import java.util.ArrayList;
 
 public class Graph {
   ArrayList<Node> nodes;
+  ArrayList<Node> noIncomingEdges = new ArrayList<Node>();
   ArrayList<Integer> topOrder = new ArrayList<Integer>();
   boolean notCycle = true;
+  int numActive = 0;
 
   public Graph() {
     this.nodes = new ArrayList<Node>();
   }
 
   public void addNode(Node n) {
+    ++numActive;
     this.nodes.add(n);
   }
 
@@ -42,55 +45,14 @@ public class Graph {
       System.out.println();
     }
   }
-
-//  public boolean topoOrder() {
-//
-//    int i;
-//    boolean result = true;
-//    // Determine incoming active edges
-//    // Set number of incoming edges from active nodes
-//
-//    // Print each iteration
-//    for (i = 0; i < nodes.size(); ++i) {
-//      int cnt = 0;
-//      for (int j = 0; j < nodes.get(i).adjlistIn.size(); ++j) {
-//        if (nodes.get(i).adjlistIn.get(j).active)
-//          ++cnt;
-//      }
-//      nodes.get(i).numInFromActive = cnt;
-//      // Print out results of edges from graph
-//      System.out.print("node ");
-//      System.out.print(nodes.get(i).toString());
-//      System.out.print(": #incoming edges from active nodes = ");
-//      System.out.println(nodes.get(i).numInFromActive);
-//      // Set all nodes to active that remain in the graph
-//      nodes.get(i).active = true;
-//    }
-//
-//    // Find starting node(s)
-//    boolean active = false;
-//    for (i = 0; i < nodes.size(); ++i) {
-//      // Delete start node and call recursively
-//      if (nodes.get(i).numInFromActive == 0 && !active) {
-//        active = true;
-//        System.out.print("Removing node: ");
-//        System.out.println(nodes.get(i).toString());
-//        nodes.get(i).active = false;
-//        nodes.remove(i);
-//      }
-//    }
-//    // Recursively call if conditions not met
-//    if (!active && nodes.size() != 0) {
-//      notCycle = false;
-//      System.out.println("No topological order found");
-//    }
-//
-//    if (active && nodes.size() != 0) {
-//      topoOrder();
-//    }
-//    return notCycle;
-//  }
-//}
+  int countInEdges(int i) {
+    int cnt = 0;
+    for (int j = 0; j < nodes.get(i).adjlistIn.size(); ++j) {
+      if (nodes.get(i).adjlistIn.get(j).active)
+        ++cnt;
+    }
+    return cnt;
+  }
 
   public void printNode(int index) {
     System.out.print("node ");
@@ -102,16 +64,12 @@ public class Graph {
   public boolean topoOrder() {
 
     int i;
-    // Determine incoming active edges
-    // Set number of incoming edges from active nodes
-
+    boolean result = true;
     for (i = 0; i < nodes.size(); ++i) {
       int cnt = 0;
-      for (int j = 0; j < nodes.get(i).adjlistIn.size(); ++j) {
-        if (nodes.get(i).adjlistIn.get(j).active)
-          ++cnt;
-      }
-      nodes.get(i).numInFromActive = cnt;
+      // Determine incoming active edges and
+      // Set number of incoming edges from active nodes
+      nodes.get(i).numInFromActive = countInEdges(i);
       // Print out results of edges from graph
       printNode(i);
       // Set all nodes to active that remain in the graph
@@ -127,14 +85,13 @@ public class Graph {
         System.out.print("Removing node: ");
         System.out.println(nodes.get(i).toString());
         nodes.get(i).active = false;
-        topOrder.add(i);
         nodes.remove(i);
       }
     }
     // Recursively call if conditions not met
     if (!active && nodes.size() != 0) {
       notCycle = false;
-      System.out.println("\n<No topological order found>");
+      System.out.println("\n<------No topological order found------>");
     }
 
     if (active && nodes.size() != 0) {
@@ -143,3 +100,56 @@ public class Graph {
     return notCycle;
   }
 }
+
+
+//  public boolean topoOrder() {
+//
+//    int i;
+//    // Determine incoming active edges
+//    // Set number of incoming edges from active nodes
+//
+//    for (i = 0; i < numActive; ++i) {
+//        if (nodes.get(i).active && nodes.get(i).adjlistIn.size() == 0)
+//          noIncomingEdges.add(nodes.get(i));
+//
+//
+//
+//
+////      int cnt = 0;
+////      for (int j = 0; j < nodes.get(i).adjlistIn.size(); ++j) {
+////        if (nodes.get(i).adjlistIn.get(j).active)
+////          ++cnt;
+////      }
+////      nodes.get(i).numInFromActive = cnt;
+//      // Print out results of edges from graph
+//      printNode(i);
+//      // Set all nodes to active that remain in the graph
+//      nodes.get(i).active = true;
+//    }
+//
+//    // Find starting node(s)
+//    boolean active = false;
+//    for (i = 0; i < nodes.size(); ++i) {
+//      // Delete start node and call recursively
+//      if (nodes.get(i).numInFromActive == 0 && !active) {
+//        active = true;
+//        System.out.print("Removing node: ");
+//        System.out.println(nodes.get(i).toString());
+//        nodes.get(i).active = false;
+//        topOrder.add(i);
+//        nodes.remove(i);
+//        --numActive;
+//      }
+//    }
+//    // Recursively call if conditions not met
+//    if (!active && nodes.size() != 0) {
+//      notCycle = false;
+//      System.out.println("\n<No topological order found>");
+//    }
+//
+//    if (active && nodes.size() != 0) {
+//      topoOrder();
+//    }
+//    return notCycle;
+//  }
+//}
